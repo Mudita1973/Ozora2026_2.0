11ecfaf5c741b05113de5250e0cec68cc4f6ce7d
Mudita1973 <131659032+Mudita1973@users.noreply.github.com>
2026-07-16 07:19:57 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-const CACHE='ozora-2026-v2.1.0';
-const ASSETS=['./','./index.html','./ozora-intro.html','./ozora.html','./ozora-meinplan.html','./ozora-manifest.json','./ozora-icon-192.png','./ozora-icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
